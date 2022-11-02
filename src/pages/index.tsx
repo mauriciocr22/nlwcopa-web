1af7cb45d83a99e9622311a48e30f35e52d20508
Maurício de Carvalho <mauriciocr223@gmail.com>
2022-11-02 19:48:17 -0300

    feat: installed tailwind and postcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
-  return <h1>Count: {props.count}</h1>;
+  return (
+    <h1 className="text-blue-800 font-bold text-4xl">Count: {props.count}</h1>
+  );
 }
 
 export const getServerSideProps = async () => {
